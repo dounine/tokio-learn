ce91c9d260f96748423387c685b43643c91cafe1
lake <102535481@qq.com>
2024-06-04 01:57:41 +0800

    init

---
+++ src/file_appender.rs
@@ -0,0 +1,329 @@
+use std::fs;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+use std::ops::Add;
+use std::path::{Path, PathBuf};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{PoisonError, RwLock};
+use chrono::{Datelike, DateTime, Local, TimeDelta, Timelike};
+use derive_builder::Builder;
+
+
+#[derive(Debug, Default, Clone)]
+pub enum Rotation {
+    #[default]
+    Daily,
+    Hourly,
+    Minutely,
+    Never,
+}
+
+impl Rotation {
+    fn next_time(&self, current_time: Time) -> Option<Time> {
+        let time = match self {
+            Rotation::Daily => {
+                Some(current_time.add(TimeDelta::days(1)))
+            }
+            Rotation::Hourly => {
+                Some(current_time.add(TimeDelta::hours(1)))
+            }
+            Rotation::Minutely => {
+                Some(current_time.add(TimeDelta::minutes(1)))
+            }
+            _ => None
+        };
+        time.and_then(|t| self.round_time(t))
+    }
+
+    fn round_time(&self, time: DateTime<Local>) -> Option<Time> {
+        match self {
+            Rotation::Daily => {
+                Some(time.with_hour(0).unwrap().with_minute(0).unwrap().with_second(0).unwrap())
+            }
+            Rotation::Hourly => {
+                Some(time.with_minute(0).unwrap().with_second(0).unwrap())
+            }
+            Rotation::Minutely => {
+                Some(time.with_second(0).unwrap())
+            }
+            _ => None
+        }
+    }
+
+    pub const DAILY: &'static str = "%Y-%m-%d";
+    pub const HOURLY: &'static str = "%Y-%m-%d-%H";
+    pub const MINUTELY: &'static str = "%Y-%m-%d-%H-%M";
+    pub const NEVER: &'static str = Self::DAILY;
+
+    fn date_format(&self) -> &'static str {
+        match self {
+            Rotation::Daily => Self::DAILY,
+            Rotation::Hourly => Self::HOURLY,
+            Rotation::Minutely => Self::MINUTELY,
+            Rotation::Never => Self::NEVER,
+        }
+    }
+}
+
+pub struct TracingFileAppender<'a> {
+    state: State<'a>,
+    writer: RwLock<File>,
+}
+
+#[derive(Default, Builder, Debug)]
+#[builder(setter(into))]
+pub struct Appender {
+    rotation: Rotation,
+    prefix: Option<String>,
+    suffix: Option<String>,
+}
+
+struct State<'a> {
+    rotation: Rotation,
+    next_time: AtomicUsize,
+    directory: &'a Path,
+    prefix: Option<String>,
+    suffix: Option<String>,
+}
+
+impl<'a> State<'a> {
+    pub fn new<'b: 'a, T: AsRef<Path> + 'b + ?Sized>(
+        now: Time,
+        rotation: Rotation,
+        directory: &'b T,
+        prefix: Option<String>,
+        suffix: Option<String>,
+    ) -> Result<(Self, RwLock<File>), anyhow::Error> {
+        let next_time = rotation.next_time(now);
+        let state = State {
+            rotation,
+            next_time: AtomicUsize::new(next_time.map(|x| x.timestamp() as usize).unwrap_or(0)),
+            directory: directory.as_ref(),
+            prefix,
+            suffix,
+        };
+        let filename = state.join_date(now);
+        let writer_file = Self::create_writer(directory.as_ref(), &filename)?;
+        let writer = RwLock::new(writer_file);
+        Ok((state, writer))
+    }
+
+    pub fn should_rollover(&self, time: Time) -> Option<usize> {
+        let next_time = self.next_time.load(Ordering::Acquire);
+        if next_time == 0 {
+            return None;
+        }
+        if time.timestamp() >= next_time as i64 {
+            return Some(next_time);
+        }
+        None
+    }
+
+    fn add_date(&self, now: Time, current_timestamp: usize) -> bool {
+        let next_time = self
+            .rotation
+            .next_time(now)
+            .map(|date| date.timestamp() as usize)
+            .unwrap_or(0);
+        let result = self.next_time
+            .compare_exchange(current_timestamp, next_time, Ordering::AcqRel, Ordering::Acquire)
+            .is_ok();
+        result
+    }
+
+    fn join_date(&self, time: Time) -> String {
+        let format = self.rotation.date_format();
+        let format_time = time.format(format);
+        match (
+            &self.rotation,
+            &self.prefix,
+            &self.suffix,
+        ) {
+            (Rotation::Never, Some(filename), None) => filename.to_string(),
+            (Rotation::Never, Some(filename), Some(suffix)) => format!("{}.{}", filename, suffix),
+            (Rotation::Never, None, Some(suffix)) => suffix.to_string(),
+            (_, Some(filename), Some(suffix)) => format!("{}.{}.{}", filename, format_time, suffix),
+            (_, Some(filename), None) => format!("{}.{}", filename, format_time),
+            (_, None, Some(suffix)) => format!("{}.{}", format_time, suffix),
+            (_, None, None) => format_time.to_string(),
+        }
+    }
+
+    fn refresh_writer(&self, now: Time, file: &mut File) {
+        let filename = self.join_date(now);
+        match Self::create_writer(&self.directory, &filename) {
+            Ok(new_file) => {
+                if let Err(err) = file.flush() {
+                    eprintln!("Couldn't flush previous writer: {}", err);
+                }
+                *file = new_file;
+            }
+            Err(err) => eprintln!("Couldn't create writer for logs: {}", err),
+        }
+    }
+    pub(crate) fn create_writer(directory: &Path, filename: &str) -> Result<File, anyhow::Error> {
+        let mut open_options = OpenOptions::new();
+        open_options.append(true);
+        open_options.create(true);
+        let path = directory.join(filename);
+        let new_file = open_options.open(path.as_path());
+        if new_file.is_err() {
+            if let Some(parent) = path.parent() {
+                fs::create_dir_all(parent)?;
+                return Ok(open_options.open(path)?);
+            }
+        }
+        new_file.map_err(|e| e.into())
+    }
+
+    #[inline]
+    pub fn now() -> Time {
+        chrono::Local::now()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::ops::Add;
+    use std::path::Path;
+    use chrono::{Datelike, DateTime, Local, NaiveDate, NaiveDateTime, TimeDelta, TimeZone};
+    use crate::file_appender::{Rotation, State};
+
+    #[test]
+    fn test_state_add_date_fail() -> Result<(), anyhow::Error> {
+        //时间未达一个小时，不能触发rollover
+        let now = Local.with_ymd_and_hms(2024, 12, 12, 12, 0, 0).unwrap();
+        let state = State::new(
+            now,
+            Rotation::Hourly,
+            "logs",
+            None,
+            None,
+        )?.0;
+        let now = now.add(TimeDelta::minutes(59));
+        let current_timestamp = state.should_rollover(now);
+        assert!(current_timestamp.is_none());
+        Ok(())
+    }
+
+    #[test]
+    fn test_state_add_date_ok() -> Result<(), anyhow::Error> {
+        //时间过去一个小时，应该触发一次rollover
+        let now = Local.with_ymd_and_hms(2024, 12, 12, 12, 0, 0).unwrap();
+        let state = State::new(
+            now,
+            Rotation::Hourly,
+            "logs",
+            None,
+            None,
+        )?.0;
+        let now = now.add(TimeDelta::hours(1));
+        let current_timestamp = state.should_rollover(now);
+        assert!(current_timestamp.is_some());
+        let current_timestamp = current_timestamp.unwrap();
+        let res = state.add_date(now, current_timestamp);
+        assert_eq!(true, res);
+        Ok(())
+    }
+
+    #[test]
+    fn test_state_join_date_format() -> Result<(), anyhow::Error> {
+        assert_eq!(State::new(
+            State::now(),
+            Rotation::Hourly,
+            "logs",
+            None,
+            None,
+        )?.0.join_date(Local.with_ymd_and_hms(2024, 12, 12, 12, 0, 0).unwrap()), "2024-12-12-12");
+        assert_eq!(State::new(
+            State::now(),
+            Rotation::Daily,
+            "logs",
+            None,
+            None,
+        )?.0.join_date(Local.with_ymd_and_hms(2024, 12, 12, 0, 0, 0).unwrap()), "2024-12-12");
+        assert_eq!(State::new(
+            State::now(),
+            Rotation::Daily,
+            "logs",
+            None,
+            Some("log".to_string()),
+        )?.0.join_date(Local.with_ymd_and_hms(2024, 12, 12, 0, 0, 0).unwrap()), "2024-12-12.log");
+        assert_eq!(State::new(
+            State::now(),
+            Rotation::Daily,
+            "logs",
+            Some("app".to_string()),
+            Some("log".to_string()),
+        )?.0.join_date(Local.with_ymd_and_hms(2024, 12, 12, 0, 0, 0).unwrap()), "app.2024-12-12.log");
+        assert_eq!(State::new(
+            State::now(),
+            Rotation::Never,
+            "logs",
+            Some("app".to_string()),
+            Some("log".to_string()),
+        )?.0.join_date(Local.with_ymd_and_hms(2024, 12, 12, 0, 0, 0).unwrap()), "app.log");
+        assert_eq!(State::new(
+            State::now(),
+            Rotation::Never,
+            "logs",
+            None,
+            Some("log".to_string()),
+        )?.0.join_date(Local.with_ymd_and_hms(2024, 12, 12, 0, 0, 0).unwrap()), "log");
+        assert_eq!(State::new(
+            State::now(),
+            Rotation::Never,
+            "logs",
+            Some("log".to_string()),
+            None,
+        )?.0.join_date(Local.with_ymd_and_hms(2024, 12, 12, 0, 0, 0).unwrap()), "log");
+        assert_eq!(State::new(
+            State::now(),
+            Rotation::Never,
+            "logs",
+            None,
+            None,
+        )?.0.join_date(Local.with_ymd_and_hms(2024, 12, 12, 0, 0, 0).unwrap()), "2024-12-12");
+        Ok(())
+    }
+}
+
+type Time = DateTime<Local>;
+
+impl<'a> TracingFileAppender<'a> {
+    pub fn from_builder<'b: 'a, T: AsRef<Path> + 'b + ?Sized>(builder: AppenderBuilder, directory: &'b T) -> Result<Self, anyhow::Error> {
+        let Appender {
+            rotation,
+            prefix,
+            suffix,
+        } = builder.build()?;
+        let now = State::now();
+        let (state, writer) = State::new(
+            now,
+            rotation,
+            directory,
+            prefix,
+            suffix,
+        )?;
+        Ok(TracingFileAppender {
+            state,
+            writer,
+        })
+    }
+}
+
+impl<'a> Write for TracingFileAppender<'a> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let now = State::now();
+        let writer = self.writer.get_mut().unwrap_or_else(PoisonError::into_inner);
+        if let Some(current_timestamp) = self.state.should_rollover(now) {
+            let _a = self.state.add_date(now, current_timestamp);
+            self.state.refresh_writer(now, writer);
+        }
+        writer.write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.writer.get_mut().unwrap_or_else(PoisonError::into_inner).flush()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,62 +3,81 @@ use std::future::Future;
 use std::time::Duration;
 
 use actix_web::{App, HttpServer, Responder};
+use actix_web::cookie::time;
+use actix_web::cookie::time::UtcOffset;
 use actix_web::web::Data;
+use time::OffsetDateTime;
+use tracing::info;
 use tracing_actix_web::TracingLogger;
+// use tracing_appender::rolling::Rotation;
 use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::fmt::time::OffsetTime;
 
-use migration::{ConnectionTrait, MigratorTrait};
+use migration::{ConnectionTrait, Migrator, MigratorTrait};
 use migration::sea_orm::{ActiveModelTrait, ConnectOptions, Database, DatabaseConnection, EntityTrait, IntoActiveModel, TransactionTrait};
+use crate::file_appender::{AppenderBuilder, Rotation};
 
 use crate::span::DomainRootSpanBuilder;
 
 pub mod span;
 pub mod store;
 pub mod router;
+pub mod file_appender;
 pub mod common;
 
 // #[tokio::main]
+// async fn main() -> Result<(), anyhow::Error> {
+//     let mut headers = Vec::new();
+//     //10个并发请求
+//     for i in 0..5 {
+//         let handle = tokio::spawn(async move {
+//             let client = reqwest::Client::builder()
+//                 .build().unwrap();
+//             let form = reqwest::multipart::Form::new()
+//                 .text("app_name", "hello")
+//                 .text("app_version", "1.0.0")
+//                 .text("app_bundle_id", "a.b.c")
+//                 .text("remove_all_plugin", "false")
+//                 .text("remove_watch_plugin", "false")
+//                 .text("remove_device_limit", "false")
+//                 .text("remove_app_jump", "false")
+//                 .text("enable_file_share", "false")
+//                 .text("zip_level", "Middle")
+//                 .part("p12_file", reqwest::multipart::Part::bytes(std::fs::read("/Users/lake/Library/Mobile Documents/com~apple~CloudDocs/证书/lake_13_pm.p12").unwrap()).file_name("lake_13_pm.p12"))
+//                 .part("mp_file", reqwest::multipart::Part::bytes(std::fs::read("/Users/lake/Library/Mobile Documents/com~apple~CloudDocs/证书/lake_13_pm.mobileprovision").unwrap()).file_name("lake_13_pm.mobileprovision"))
+//                 .text("p12_password", "1");
+//
+//             let request = client.request(reqwest::Method::POST, format!("http://192.168.0.85:3001/api/ipa/sign/ZxJrE/{}", i))
+//                 .multipart(form);
+//
+//             let response = request.send().await.unwrap();
+//             let body = response.text().await.unwrap();
+//
+//             println!("{}", body);
+//         });
+//         headers.push(handle);
+//     }
+//     for handle in headers {
+//         handle.await?;
+//     }
+//     Ok(())
+// }
+
+
 #[actix_web::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let mut headers = Vec::new();
-    //10个并发请求
-    for i in 0..5 {
-        let handle = tokio::spawn(async move {
-            let client = reqwest::Client::builder()
-                .build().unwrap();
-            let form = reqwest::multipart::Form::new()
-                .text("app_name", "hello")
-                .text("app_version", "1.0.0")
-                .text("app_bundle_id", "a.b.c")
-                .text("remove_all_plugin", "false")
-                .text("remove_watch_plugin", "false")
-                .text("remove_device_limit", "false")
-                .text("remove_app_jump", "false")
-                .text("enable_file_share", "false")
-                .text("zip_level", "Middle")
-                .part("p12_file", reqwest::multipart::Part::bytes(std::fs::read("/Users/lake/Library/Mobile Documents/com~apple~CloudDocs/证书/lake_13_pm.p12").unwrap()).file_name("lake_13_pm.p12"))
-                .part("mp_file", reqwest::multipart::Part::bytes(std::fs::read("/Users/lake/Library/Mobile Documents/com~apple~CloudDocs/证书/lake_13_pm.mobileprovision").unwrap()).file_name("lake_13_pm.mobileprovision"))
-                .text("p12_password", "1");
-
-            let request = client.request(reqwest::Method::POST, format!("http://192.168.0.85:3001/api/ipa/sign/ZxJrE/{}", i))
-                .multipart(form);
-
-            let response = request.send().await.unwrap();
-            let body = response.text().await.unwrap();
-
-            println!("{}", body);
-        });
-        headers.push(handle);
-    }
-    for handle in headers {
-        handle.await?;
-    }
-    Ok(())
-}
-
-async fn _main() -> Result<(), anyhow::Error> {
     dotenvy::dotenv().ok();
+    let time_offset =
+        UtcOffset::current_local_offset().unwrap_or_else(|_| UtcOffset::from_hms(8, 0, 0).unwrap());
+    let local_time = OffsetTime::new(
+        time_offset,
+        time::macros::format_description!(
+            "[year]-[month]-[day] [hour]:[minute]:[second].[subsecond digits:3]"
+        ),
+    );
+
     let format = tracing_subscriber::fmt::format()
+        .with_timer(local_time.clone())
         .with_level(true)
         .with_line_number(true)
         .with_thread_names(true)
@@ -68,7 +87,29 @@ async fn _main() -> Result<(), anyhow::Error> {
         .with_max_level(tracing::Level::INFO)
         .event_format(format);
 
-    sub.init();
+    // let file_appender = tracing_appender::rolling::Builder::new()
+    //     .filename_prefix("")
+    //     .filename_suffix("log")
+    //     .rotation(Rotation::DAILY)
+    //     .build("logs").unwrap();
+    // let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    let builder = AppenderBuilder::default()
+        .rotation(Rotation::Daily)
+        .prefix(None)
+        .suffix(Some(".log2".to_string()))
+        .clone();
+    let tracing_file_appender = file_appender::TracingFileAppender::from_builder(
+        builder,
+        "logs",
+    )?;
+    let (my_non_blocking, _guard) = tracing_appender::non_blocking(tracing_file_appender);
+
+    sub
+        // .with_writer(non_blocking) //正式环境使用
+        .with_writer(my_non_blocking)
+        .with_timer(local_time)
+        .with_ansi(false)
+        .init();
 
     let db_url = "postgres://postgres:postgres@localhost:5432/tokio-test";
     let mut opt = ConnectOptions::new(db_url);
@@ -83,7 +124,8 @@ async fn _main() -> Result<(), anyhow::Error> {
     let conn: DatabaseConnection = Database::connect(opt)
         .await
         .expect("Cannot connect to database");
-    // Migrator::up(&conn, None).await?;
+    info!("hello");
+    Migrator::up(&conn, None).await?;
 
     // let (tx, rx) = tokio::sync::mpsc::channel::<()>(10);
     //
@@ -133,18 +175,15 @@ async fn _main() -> Result<(), anyhow::Error> {
     // debug!("success");
 
     let arc_conn = Data::new(conn);
-
-
     let server = HttpServer::new(move || {
         let app = App::new();
         let tracing = TracingLogger::<DomainRootSpanBuilder>::new();
         app.wrap(tracing).app_data(arc_conn.clone())
             .service(router::index)
     });
-
     server
-        .workers(1)
-        .bind("0.0.0.0:8080")?
+        // .workers(1)
+        .bind("0.0.0.0:4000")?
         .run().await?;
 
     Ok(())
